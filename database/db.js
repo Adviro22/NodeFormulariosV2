d01184e4256df9a9e400567623b49f611a345b21
adviro22 <ajosueramos@gmail.com>
2023-07-08 14:11:05 -0500

    Arreglos

--- database/db.js
+++ database/db.js
@@ -4,7 +4,9 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'root',
-  database: process.env.DB_DATABASE || 'login_NodeJs'
+  database: process.env.DB_DATABASE || 'login_NodeJs',
+  port: process.env.DB_PORT || 3306
+
 });
 
 connection.connect((error) => {
