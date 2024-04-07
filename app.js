2d10e5e7b7af826949fc359b4b896490298abd11
adviro22 <ajosueramos@gmail.com>
2024-04-07 00:04:26 -0500

    Modificaciones de usuairos

--- app.js
+++ app.js
@@ -89,6 +89,10 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+// app.get("/register", (req, res) => {
+//   res.render("register");
+// });
+
 app.get("/index", auth, (req, res) => {
   res.render("index");
 });
