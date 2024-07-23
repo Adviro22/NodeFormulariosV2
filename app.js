30e431ceda16e9c22671cc0c0dcf7592007f8494
adviro22 <ajosueramos@gmail.com>
2024-07-23 10:45:05 -0500

    Formulario 26 base

--- app.js
+++ app.js
@@ -265,6 +265,10 @@ app.get("/formulario25", auth, (req, res) => {
   res.render("formulario25");
 });
 
+app.get("/formulario26", auth, (req, res) => {
+  res.render("formulario26");
+});
+
 //Ver Datos del usuario logeado
 app.get("/user_data", auth, (req, res) => {
   const userId = req.session.userId;
